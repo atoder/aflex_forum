77b98b7139e0bd98dfa56394e3a8c8ef73768c77
atoder <alexandert@gmail.com>
2015-12-22 16:52:12 -0800

    Stylesheets, post, devise users, etc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,25 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def show
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
       render 'new'
     end
   end
-  
+
   def edit
   end
 
